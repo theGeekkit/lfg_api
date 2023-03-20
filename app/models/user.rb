4d85d3936ff14aaeea68f63b7f10d4afaf847f9e
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2023-03-20 16:56:16 -0500

    vs code is the dumb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  def change
-    add_column :users, :jti, :string, null: false
-    add_index :users, :jti, unique: true
-# If you already have user records, you will need to initialize its `jti` column before setting it to not nullable. Your migration will look this way:
-# add_column :users, :jti, :string
-# User.all.each { |user| user.update_column(:jti, SecureRandom.uuid) }
-# change_column_null :users, :jti, false
-# add_index :users, :jti, unique: true
-  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -29,16 +29,7 @@ Devise.setup do |config|
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
-  config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
-    jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
-    ]
-    jwt.revocation_requests = [
-      ['DELETE', %r{^/logout$}]
-    ]
-    jwt.expiration_time = 30.minutes.to_i
-end
+
 
   # Configure the parent class responsible to send e-mails.
   # config.parent_mailer = 'ActionMailer::Base'
@@ -321,4 +312,15 @@ end
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 30.minutes.to_i
+  end
 end
